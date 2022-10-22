d0e669cfc7ae8983856c2117782618ac17c6cfb5
Reece Bradley <naruto73@hotmail.co.uk>
2022-10-22 12:16:04 +0100

    create funtion to play one round of the game

--- script.js
+++ script.js
@@ -3,6 +3,19 @@ function getComputerChoice() {
   var choices = ["Rock", "Paper", "Scissors"];
   return choices[Math.floor(Math.random() * choices.length)];
 }
-//store chosen word in a variable
-var choice = getComputerChoice();
-console.log(choice);
+//create function to play one round of the game
+function playRound(playerSelection, computerSelection) {
+  if(playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+    return "It's a tie! Pick again";
+  } else {
+    if(playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors") {
+      return "You Win! Rock beats Scissors";
+    } else {
+      return "You Lose! Paper beats Rock";
+    }
+  }
+}
+
+var playerSelection = "rock";
+var computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
