96650f43eb6c73d734b48d9a6de67df67a49d6bd
Reece Bradley <naruto73@hotmail.co.uk>
2022-11-07 16:48:01 +0000

    Update code and add a UI

--- script.js
+++ script.js
@@ -1,62 +1,87 @@
+const selectionButtons = document.querySelectorAll('[data-selection]');
+const computerScore = document.querySelector('[data-computer-score]');
+const yourScore = document.querySelector('[data-your-score]');
+const choiceResult = document.querySelector('[data-results]');
+const resetBtn = document.querySelector('#reset');
+
+// create a button to reset the game
+resetBtn.addEventListener('click', () => location.reload());
+
+var user = 0;
+var computer = 0;
+
+// use addEventListener to check which button the user has selected
+selectionButtons.forEach(selectionButton => {
+  selectionButton.addEventListener('click', (e) => {
+    const selectionName = selectionButton.dataset.selection;
+    playerSelection = selectionName;
+    var computerSelection = getComputerChoice();
+    
+    playRound(playerSelection, computerSelection);
+  })
+});
+
 // create function to randomly pick a word from an array
 function getComputerChoice() {
-  var choices = ["Rock", "Paper", "Scissors"];
+  var choices = ["rock", "paper", "scissors"];
   return choices[Math.floor(Math.random() * choices.length)];
 }
+
 // create function to play one round of the game
 function playRound(playerSelection, computerSelection) {
-  if(playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
-    alert("It's a tie! Pick again");
+  if(playerSelection === computerSelection) {
+    choiceResult.textContent = "It's a tie! Pick again";
   } else {
-    if(playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors") {
-      user++
-      alert("You Win! Rock beats Scissors");
+    if(playerSelection === "rock" && computerSelection === "scissors") {
+      yourScore.textContent = ++user
+      choiceResult.textContent = "You Win! Rock beats Scissors";
     } else {
-      if(playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "paper") {
-        computer++
-        alert("You Lose! Paper beats Rock");
+      if(playerSelection === "rock" && computerSelection === "paper") {
+        computerScore.textContent = ++computer
+        choiceResult.textContent = "You Lose! Paper beats Rock";
       } else {
-        if(playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock") {
-          user++
-          alert("You Win! Paper beats Rock");
+        if(playerSelection === "paper" && computerSelection === "rock") {
+          yourScore.textContent = ++user
+          choiceResult.textContent = "You Win! Paper beats Rock";
         } else {
-          if(playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "scissors") {
-            computer++
-            alert("You Lose! Scissors beat Paper");
+          if(playerSelection === "paper" && computerSelection === "scissors") {
+            computerScore.textContent = ++computer
+            choiceResult.textContent = "You Lose! Scissors beat Paper";
           } else {
-            if(playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper") {
-              user++
-              alert("You Win! Scissors beat Paper");
+            if(playerSelection === "scissors" && computerSelection === "paper") {
+              yourScore.textContent = ++user
+              choiceResult.textContent = "You Win! Scissors beat Paper";
             } else {
-              computer++
-              alert("You Lose! Rock beats Scissors");
+              computerScore.textContent = ++computer
+              choiceResult.textContent = "You Lose! Rock beats Scissors";
             }
           }
         }
       }
     }
-  } 
-}
-
-var user = 0;
-var computer = 0;
-var gameNum = 0;
-
-// create function to run the game 5 times and to keep score of the results
-function game() {
-  var playerSelection = prompt("Choose Rock, Paper or Scissors.");
-  var computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
-  gameNum++;
-  if(gameNum !== 5) {
-    game();
-  }  else if (user > computer) {
-    alert("Game Over. You Won! You scored " + user + ", the computer scored " + computer + ".");
-  } else  if (computer > user) {
-    alert("Game Over. You Lost! You scored " + user + ", the computer scored " + computer + ".");
-  } else {
-    alert("Game Over. It's a Tie!");
   }
+  game();
 }
+
+// create function to disable the buttons once the game is over
+function disableBtns() {
+  selectionButtons.forEach(elem => {
+    elem.disabled = true;
+  })
+};
+
+// create function to run the game until someone wins
+function game() {
+  if ((user !== 5) && (computer !== 5)) {
+  } else if (user === 5 && computer !== 5) {
+    choiceResult.textContent = "Game Over. You Won! You scored " + user + ", the computer scored " + computer + ".";
+    disableBtns();
+  } else if (computer === 5 && user !== 5) {
+    choiceResult.textContent = "Game Over. You Lost! You scored " + user + ", the computer scored " + computer + ".";
+    disableBtns();
+  } else {
+    playRound();
+  };
+}; 
 
 game();
